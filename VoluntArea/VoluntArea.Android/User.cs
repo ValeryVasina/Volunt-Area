f2760788e74c83ff67f6d4f4e0562fce96b7aff2
Sofia Bifaciatus <sofiya.yampolskaya@gmail.com>
2019-12-13 22:03:16 +0300

    Adding User Class and json file

---
+++ VoluntArea/VoluntArea.Android/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace VoluntArea.Droid
+{
+    class User
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+
+    }
+}
